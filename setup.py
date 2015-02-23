de1505d0eb610ca7a424a2834ef9bc31b893eb49
Takaaki AOKI <aoki.takaaki.6v@kyoto-u.ac.jp>
2015-02-23 18:02:09 +0900

    modification on setup.py and REDME.md

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
-from setuptool import setup, find_packages
+import os
+from setuptools import setup, find_packages
 
-ver_file = os.path.join('marlowe_ui', 'version.py')
+ver_file = os.path.join('ofblockmeshdicthelper', 'version.py')
 vars = {}
 exec(open(ver_file).read(), vars)
 
@@ -12,15 +13,15 @@ setup(
     author='Takaaki AOKI',
     author_email='aoki.takaaki@gmail.com',
     url='https://github.com/takaakiaoki/ofblockmeshdicthelper',
-    parckages=find_packages(),
+    packages=find_packages(),
     package_dir={'ofblockmeshdicthelper': 'ofblockmeshdicthelper'},
     classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 2.7",
         "License :: OSI Approved :: MIT License",
         "Development Status :: 4 - Beta",
         "Environment :: Other Environment",
         "Intended Audience :: Science/Research",
         "Operating System :: OS Independent",
-        "Operating System :: Microsoft :: Windows :: Windows 7",
         "Topic :: Scientific/Engineering :: Physics"])
