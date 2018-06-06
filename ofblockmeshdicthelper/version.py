4fbcdb6cb04eac29a66a62649a6df1636166d715
Takaaki AOKI <aoki.takaaki.6v@kyoto-u.ac.jp>
2018-06-06 13:03:17 +0900

    update version number

--- ofblockmeshdicthelper/version.py
+++ ofblockmeshdicthelper/version.py
@@ -1,6 +1,6 @@
 _version_major = 0
-_version_minor = 1
-_version_micro = 9
+_version_minor = 2
+_version_micro = 0
 
 __version__ = "{0:d}.{1:d}.{2:d}".format(
     _version_major, _version_minor, _version_micro)
